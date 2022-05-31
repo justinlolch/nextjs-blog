c63384d9806a34ac1a08e60618c80d305552b0e7
Justin Lo <kk123456235@gmail.com>
2022-05-31 20:12:47 +0800

    Add phase in next config 1. use different database in different environment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      env: {
+        mongodb_username: process.env.DB_USER,
+        mongodb_password: process.env.DB_USER_PASSWORD,
+        mongodb_host: process.env.DB_HOST,
+        mongodb_database: process.env.DB_DATABASE,
+      },
+    };
+  }
+
+  return {
+    reactStrictMode: true,
+    env: {
+      mongodb_username: process.env.DB_USER,
+      mongodb_password: process.env.DB_USER_PASSWORD,
+      mongodb_host: process.env.DB_HOST,
+      mongodb_database: process.env.DB_DATABASE_PRODUCTION,
+    },
+  };
+};
+
+module.exports = nextConfig;
