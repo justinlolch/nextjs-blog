b048e192c9764d969cd1f706516def061150d858
Justin Lo <kk123456235@gmail.com>
2022-05-31 11:13:45 +0800

    1. add two components in Home page 2. add css file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,12 @@
-export default function Home() {}
+import { Fragment } from "react"
+
+export default function Home() {
+  return
+  <Fragment>
+    {/* <Hero/> */}
+    {/* <FeaturedPosts/> */}
+  </Fragment>
+}
+
+// 1) Hero => Present ourseleves
+// 2) Featured Posts
