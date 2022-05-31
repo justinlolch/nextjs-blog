f076784f7bbe0ecc6fd8adee8983c187fd554afc
Justin Lo <kk123456235@gmail.com>
2022-05-31 14:04:44 +0800

    Rendering MarkDown 1. install ReactMarkdown lib 2. render Markdown as JSX

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -1,4 +1,5 @@
 import classes from "../../../styles/post-content.module.css";
+import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 
 const DUMMY_POST = {
@@ -6,15 +7,15 @@ const DUMMY_POST = {
   slug: "post-1",
   date: "2020-01-01",
   image: "getting-started-nextjs.png",
-  content: '# This is a first post',
+  content: "# This is a first post",
 };
 
 export default function PostContent(props) {
-    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
