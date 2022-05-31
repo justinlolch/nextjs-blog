775e1b25ac1ab57f8a98317e836b3f6e7b46056d
Justin Lo <kk123456235@gmail.com>
2022-05-31 16:35:05 +0800

    Storing messages to mongodb 1. install mongodb packages 2. save messages to db

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,36 +1,52 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
-    post(req, res);
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      return res.status(422).json({
+        message: "Invalid input.",
+      });
+    }
+  
+    // Store it in a database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+  
+    let client;
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@udemy.tkl9f.mongodb.net/my-blog?retryWrites=true&w=majority`
+      );
+    } catch (error) {
+      return res.status(500).json({ message: "Could not connect to database" });
+    }
+  
+    const db = client.db();
+  
+    try {
+      const res = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = res.insertedId;
+    } catch (error) {
+      client.close();
+      return res.status(500).json({ message: "Could not store message" });
+    }
+  
+    client.close();
+  
+    return res
+      .status(201)
+      .json({ message: "Message sent successfully.", newMessage: newMessage });
   }
-}
-
-function post(req, res) {
-  const { email, name, message } = req.body;
-
-  if (
-    !email ||
-    !email.includes("@") ||
-    !name ||
-    name.trim() === "" ||
-    !message ||
-    message.trim() === ""
-  ) {
-    res.status(422).json({
-      message: "Invalid input.",
-    });
-    return;
-  }
-
-  // Store it in a database
-  const newMessage = {
-    email,
-    name,
-    message,
-  };
-
-  console.log(newMessage);
-
-  res
-    .status(201)
-    .json({ message: "Message sent successfully.", newMessage: newMessage });
 }
